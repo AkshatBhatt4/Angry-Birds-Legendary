68b4a2a925c59dfed7861e81afdd16a0a97b6511
Akshat Bhatt <163007260+AkshatBhatt4@users.noreply.github.com>
2024-11-29 22:32:21 +0530

    Update Level.java

--- core/src/main/java/com/AngryBird/game/Level.java
+++ core/src/main/java/com/AngryBird/game/Level.java
@@ -29,7 +29,7 @@ public abstract class Level {
     protected boolean hasWon;
     protected Texture looseButton;
     protected Rectangle looseButtonBounds;
-    protected boolean haslost;
+    protected boolean hasLost;
     public int ground=65;
     public float tower1X = 550;
 
@@ -45,23 +45,20 @@ public abstract class Level {
         this.saveButton = new Texture("SaveButton.png");
         this.levelNumber = levelNumber;
 
-        // Position pause/play button in top right corner
         this.pauseButtonBounds = new Rectangle(730, 420, 50, 50);
 
-        // Position menu items directly below the pause button with small gaps
         this.quitButtonBounds = new Rectangle(730, 300, 50, 50);
         this.musicToggleBounds = new Rectangle(730, 360, 42, 42);
 
-        // Position slingshot
         this.slingshotBounds = new Rectangle(130, ground, 100, 85);
-        this.winButton = new Texture("win.png"); // Using your win.png
-        this.winButtonBounds = new Rectangle(20, 420, 50, 50); // Top left position
+        //this.winButton = new Texture("win.png"); // Using your win.png
+        //this.winButtonBounds = new Rectangle(160, 420, 50, 50); // Top left position
         this.hasWon = false;
-        this.saveButtonBounds = new Rectangle(160, 420, 50, 50);
+        this.saveButtonBounds = new Rectangle(20, 420, 50, 50);
 
-        this.looseButton = new Texture("lose.png"); // Using your win.png
-        this.looseButtonBounds = new Rectangle(90, 420, 50, 50); // Top left position
-        this.haslost = false;
+        //this.looseButton = new Texture("lose.png"); // Using your win.png
+        //this.looseButtonBounds = new Rectangle(90, 420, 50, 50); // Top left position
+        this.hasLost = false;
 
         this.isPaused = false;
         this.shouldReturnToLevelSelect = false;
@@ -69,33 +66,21 @@ public abstract class Level {
     }
 
     public void render(SpriteBatch batch, boolean isMusicOn) {
-        // Draw background
         batch.draw(background, 0, 0, 800, 480);
-
-        // Draw level content if not paused
         if (!isPaused) {
             renderLevelContent(batch);
 
-            batch.draw(winButton, winButtonBounds.x, winButtonBounds.y, winButtonBounds.width, winButtonBounds.height);
+//            batch.draw(winButton, winButtonBounds.x, winButtonBounds.y, winButtonBounds.width, winButtonBounds.height);
             batch.draw(saveButton, saveButtonBounds.x, saveButtonBounds.y, saveButtonBounds.width, saveButtonBounds.height);
-            batch.draw(looseButton, looseButtonBounds.x, looseButtonBounds.y, looseButtonBounds.width, looseButtonBounds.height);
+//            batch.draw(looseButton, looseButtonBounds.x, looseButtonBounds.y, looseButtonBounds.width, looseButtonBounds.height);
+            batch.draw(slingshot, slingshotBounds.x, slingshotBounds.y, slingshotBounds.width, slingshotBounds.height);
         }
-
-        // Always draw the slingshot, regardless of pause state
-        batch.draw(slingshot, slingshotBounds.x, slingshotBounds.y, slingshotBounds.width, slingshotBounds.height);
-
-        // Draw pause/play button based on current state
         if (isPaused) {
-            // Draw play button when paused
             batch.draw(playButton, pauseButtonBounds.x, pauseButtonBounds.y, pauseButtonBounds.width, pauseButtonBounds.height);
-
-            // Draw menu items
             batch.draw(quitButton, quitButtonBounds.x, quitButtonBounds.y, quitButtonBounds.width, quitButtonBounds.height);
-
             batch.draw(isMusicOn ? musicOn : musicOff, musicToggleBounds.x, musicToggleBounds.y, musicToggleBounds.width, musicToggleBounds.height);
         }
         else {
-            // Draw pause button when playing
             batch.draw(pauseButton, pauseButtonBounds.x, pauseButtonBounds.y, pauseButtonBounds.width, pauseButtonBounds.height);
         }
     }
@@ -114,20 +99,21 @@ public abstract class Level {
             } else if (musicToggleBounds.contains(touchX, touchY)) {
                 musicStateChanged = true;
             }
-        }else if (winButtonBounds.contains(touchX, touchY)) {
-            hasWon = true;
         }
-        else if (looseButtonBounds.contains(touchX, touchY)) {
-            haslost = true;
-        }
+//        else if (winButtonBounds.contains(touchX, touchY)) {
+//            hasWon = true;
+//        }
+//        else if (looseButtonBounds.contains(touchX, touchY)) {
+//            hasLost = true;
+//        }
     }
 
     public boolean hasWon() {
         return hasWon;
     }
 
-    public boolean haslost() {
-        return haslost;
+    public boolean hasLost() {
+        return hasLost;
     }
 
     protected abstract void renderLevelContent(SpriteBatch batch);
